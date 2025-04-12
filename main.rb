6812ea92e10fdf06811c13a7504645c0788f5d36
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-04-12 18:14:53 +0000

    Finish phase 1b

--- main.rb
+++ main.rb
@@ -2,6 +2,14 @@
 require 'socket'
 require 'json'
 
+def with_sockets(ports)
+  ports.each do |port|
+    socket = TCPSocket.new('localhost', port)
+    yield(socket)
+    socket.close
+  end
+end
+
 quorum_ports = []
 5.times do |index|
   quorum_ports << 5000 + index
@@ -9,10 +17,26 @@ end
 quorum_ports.map do |port|
   fork do
     server = TCPServer.new('localhost', port)
+    promise_number = 0
+    proposers_ids = []
+    ## Store highest proposal(value, number)
     loop do
       client = server.accept
-
-      client.puts("Go with the message from port #{port}")
+      request = client.gets
+      data = JSON.parse(request)
+      case data['action']
+      when 'prepare'
+        proposer_id = data['proposer_id']
+        if proposer_id > promise_number 
+          promise_number = proposer_id
+          message = {accepted: true, promise_number: proposer_id}.to_json
+          client.puts(message)
+          proposers_ids << proposer_id
+        else
+          message = {accepted: false}.to_json
+          client.puts(message)
+        end
+      end
       client.close
     end
   end
@@ -26,16 +50,28 @@ end
 proposer_ports.map do |port|
   fork do
     server = TCPServer.new('localhost', port)
+    proposer_id = 1
+    ## Add async to proposer communication
     loop do
       client = server.accept
       request = client.gets
       data = JSON.parse(request)
       case data['action']
+      when 'prepare'
+        acceptors_responses = []
+        proposer_id += 1
+        message = { proposer_id: proposer_id, action: 'prepare' }.to_json
+        with_sockets(quorum_ports) do |acceptor|
+          acceptor.puts(message)
+          response = acceptor.gets
+          acceptors_responses << response
+          acceptor.close
+        end
       when 'create_message'
         quorum_ports.each do |quorum_port|
           quorum = TCPSocket.new('localhost', quorum_port)
           quorum.puts(request)
-          line = quorum.gets
+          quorum.gets
           quorum.close
         end
       end
@@ -43,7 +79,7 @@ proposer_ports.map do |port|
   end
 end
 
-message = {action: "create_message", body: "tu-tu-ru"}.to_json
+message = { action: 'prepare' }.to_json
 proposer_ports.each do |port|
   proposer = TCPSocket.new('localhost', port)
   proposer.puts(message)
