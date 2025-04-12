1978b4e15d56c2809cdddfc96466d964e2899080
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-04-12 02:06:38 +0000

    Add communication between quorum and proposer

--- main.rb
+++ main.rb
@@ -1,36 +1,51 @@
 ### TCP SOCKET
 require 'socket'
+require 'json'
 
-i = 1
-ports = []
+quorum_ports = []
 5.times do |index|
-  ports << 5000 + index
+  quorum_ports << 5000 + index
 end
-p ports
-ports.map do |port|
+quorum_ports.map do |port|
   fork do
     server = TCPServer.new('localhost', port)
-    i += 1
-    pid = Process.pid
     loop do
-      p "Starting server at process #{pid}"
       client = server.accept
-      response = "HTTP/1.1 200 OK\r\n" +
-        "Content-Type: text/plain\r\n" +
-        "Connection: close\r\n" +
-        "\r\n" +
-        "TU-TU-RU! from #{pid} port #{port}"
 
-      client.puts(response)
+      client.puts("Go with the message from port #{port}")
       client.close
     end
   end
 end
 
-ports.each do |port|
-  socket = TCPSocket.new('localhost', port)
-  while line = socket.gets
-    puts line
+proposer_ports = []
+2.times do |index|
+  proposer_ports << 3000 + index
+end
+
+proposer_ports.map do |port|
+  fork do
+    server = TCPServer.new('localhost', port)
+    loop do
+      client = server.accept
+      request = client.gets
+      data = JSON.parse(request)
+      case data['action']
+      when 'create_message'
+        quorum_ports.each do |quorum_port|
+          quorum = TCPSocket.new('localhost', quorum_port)
+          quorum.puts(request)
+          line = quorum.gets
+          quorum.close
+        end
+      end
+    end
   end
-  socket.close
+end
+
+message = {action: "create_message", body: "tu-tu-ru"}.to_json
+proposer_ports.each do |port|
+  proposer = TCPSocket.new('localhost', port)
+  proposer.puts(message)
+  proposer.close
 end
