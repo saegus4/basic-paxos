ac02488047dc91c2f8d22ed444690481a515d090
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-04-14 21:31:32 +0000

    Add promises parser and better tcp server handling

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-### TCP SOCKET
 require 'socket'
 require 'json'
 
@@ -10,78 +9,106 @@ def with_sockets(ports)
   end
 end
 
-quorum_ports = []
-5.times do |index|
-  quorum_ports << 5000 + index
-end
-quorum_ports.map do |port|
+quorum_ports = (5000..5004).to_a
+acceptor_pids = quorum_ports.map do |port|
   fork do
     server = TCPServer.new('localhost', port)
+    at_exit { server.close }
+
     promise_number = 0
-    proposers_ids = []
-    ## Store highest proposal(value, number)
+    highest_proposal = nil
+
     loop do
       client = server.accept
       request = client.gets
       data = JSON.parse(request)
+
       case data['action']
       when 'prepare'
         proposer_id = data['proposer_id']
-        if proposer_id > promise_number 
+        if proposer_id > promise_number
+          highest_proposal = [proposer_id, data['value']] if data['value']
           promise_number = proposer_id
-          message = {accepted: true, promise_number: proposer_id}.to_json
-          client.puts(message)
-          proposers_ids << proposer_id
+          response = {
+            promise: true,
+            promise_number: proposer_id,
+            highest_proposal: highest_proposal
+          }
         else
-          message = {accepted: false}.to_json
-          client.puts(message)
+          response = { promise: false }
         end
+        client.puts(response.to_json)
+      else
+        puts "#{data}"
       end
+
       client.close
     end
   end
 end
 
-proposer_ports = []
-2.times do |index|
-  proposer_ports << 3000 + index
-end
-
-proposer_ports.map do |port|
+proposer_ports = (3000..3001).to_a
+proposer_pids = proposer_ports.map do |port|
   fork do
     server = TCPServer.new('localhost', port)
+    at_exit { server.close }
+
     proposer_id = 1
-    ## Add async to proposer communication
+
     loop do
       client = server.accept
       request = client.gets
       data = JSON.parse(request)
+      data_value = data['value']
+
       case data['action']
       when 'prepare'
-        acceptors_responses = []
         proposer_id += 1
+        acceptors_responses = []
+
         message = { proposer_id: proposer_id, action: 'prepare' }.to_json
         with_sockets(quorum_ports) do |acceptor|
-          acceptor.puts(message)
-          response = acceptor.gets
-          acceptors_responses << response
-          acceptor.close
+          begin
+            acceptor.puts(message)
+            response = acceptor.gets
+            acceptors_responses << JSON.parse(response)
+          rescue => e
+            puts "Failed to contact acceptor: #{e.message}"
+          end
         end
-      when 'create_message'
-        quorum_ports.each do |quorum_port|
-          quorum = TCPSocket.new('localhost', quorum_port)
-          quorum.puts(request)
-          quorum.gets
-          quorum.close
+
+        quorum = acceptors_responses.select { |r| r['promise'] }
+        highest = quorum.max_by { |r| r['promise_number'] || 0 }
+
+        value = if highest && highest['highest_proposal']
+                  highest['highest_proposal'][1]
+                else
+                  data_value
+                end
+
+        proposal_msg = { proposal_id: proposer_id, value: value }.to_json
+        p 'Sending proposal:', proposal_msg
+
+        with_sockets(quorum_ports) do |acceptor|
+          begin
+            acceptor.puts(proposal_msg)
+          rescue => e
+            puts "Failed to send proposal: #{e.message}"
+          end
         end
       end
+
+      client.close
     end
   end
 end
 
-message = { action: 'prepare' }.to_json
+message = { action: 'prepare', value: 50 }.to_json
 proposer_ports.each do |port|
   proposer = TCPSocket.new('localhost', port)
   proposer.puts(message)
   proposer.close
 end
+
+Process.waitall
+
